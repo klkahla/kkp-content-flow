de351405e4643fd8baf3f73c8927218b8067e292
Katy Kahla <katykahla@gmail.com>
2024-08-15 15:24:17 -0600

    right column

--- kkp-content-flow-ui/src/App.tsx
+++ kkp-content-flow-ui/src/App.tsx
@@ -1,9 +1,16 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FolderPicker from './components/FolderPicker.tsx';
 
 function App() {
+  const [output, setOutput] = useState('');
+
+  const handleSubmit = () => {
+    // For demonstration, we'll just set a static output
+    setOutput('This is the output from the Submit button.');
+  };
+
   return (
     <div className="App">
       <div className="content">
@@ -13,11 +20,13 @@ function App() {
             <FolderPicker />
           </div>
           <div className="button-container">
-            <button>Submit</button>
+            <button onClick={handleSubmit}>Submit</button>
           </div>
         </div>
         <div className="right-column">
-          
+          <div className="output-panel">
+            {output}
+          </div>
         </div>
       </div>
     </div>
