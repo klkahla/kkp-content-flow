43c8c28b9a207209d92206450eba11386b1cd465
Katy Kahla <katykahla@gmail.com>
2025-04-18 10:23:14 -0600

    make folder picker a path input
    don't want to implement file upload

--- kkp-content-flow-ui/src/components/FolderPicker.tsx
+++ kkp-content-flow-ui/src/components/FolderPicker.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 
 const FolderPicker: React.FC = ({ onPathChange }) => {
   const handleFolderSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    let file_path = "";
-    if (files && files.length > 0) {
-      file_path = files[0].webkitRelativePath || files[0].name;
-      console.log('Selected file path:', file_path);
-    }
-    onPathChange("/Volumes/KKP.2024/Photos/Wedding/Blum.06.01/Finals/SEO.Web/" + file_path);
+    onPathChange(event.target.value);
   };
 
   return (
     <div className="folder-picker-container">
-        <label htmlFor="folderPicker">Choose an Alt Text SEO file:</label>
-        <input
-          type="file"
-          accept=".csv"
-          onChange={handleFolderSelect}
-        />
+      <label htmlFor="folderPicker">Choose an Alt Text SEO file:</label>
+      <input
+        type="text"
+        placeholder='Enter file path'
+        onChange={handleFolderSelect}
+        style={{ width: '100%' }}
+      />
     </div>
   );
 };
